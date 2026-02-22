ccaa0599339f2bcf8d2e9c931c5ee5e89b4db97a
Yuukub <sarnyou@gmail.com>
2026-02-23 00:46:16 +0700

    อปเดตระบบสราง sitemap และแกไขคำผดในบทความ

--- scripts/generate-sitemap.mjs
+++ scripts/generate-sitemap.mjs
@@ -18,6 +18,22 @@ const postEntries = postFiles.map((fileName) => {
   </url>`;
 });
 
+const toolsDir = path.join(process.cwd(), "src", "app", "tools");
+let toolEntries = [];
+if (fs.existsSync(toolsDir)) {
+  const toolDirs = fs.readdirSync(toolsDir, { withFileTypes: true })
+    .filter((dirent) => dirent.isDirectory())
+    .map((dirent) => dirent.name);
+
+  toolEntries = toolDirs.map((toolSlug) => {
+    const priority = toolSlug === "image-converter" ? "0.9" : "0.8";
+    return `  <url>
+    <loc>${SITE_URL}/tools/${toolSlug}/</loc>
+    <priority>${priority}</priority>
+  </url>`;
+  });
+}
+
 const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
   <url>
@@ -40,25 +56,10 @@ const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
     <loc>${SITE_URL}/tools/</loc>
     <priority>0.9</priority>
   </url>
-  <url>
-    <loc>${SITE_URL}/tools/image-converter/</loc>
-    <priority>0.9</priority>
-  </url>
-  <url>
-    <loc>${SITE_URL}/tools/xmlrpc-checker/</loc>
-    <priority>0.8</priority>
-  </url>
-  <url>
-    <loc>${SITE_URL}/tools/hash-generator/</loc>
-    <priority>0.8</priority>
-  </url>
-  <url>
-    <loc>${SITE_URL}/tools/password-generator/</loc>
-    <priority>0.8</priority>
-  </url>
+${toolEntries.join("\n")}
 ${postEntries.join("\n")}
 </urlset>
 `;
 
 fs.writeFileSync(path.join(process.cwd(), "public", "sitemap.xml"), sitemap);
-console.log(`Sitemap generated with ${postEntries.length} blog posts.`);
+console.log(`Sitemap generated with ${postEntries.length} blog posts and ${toolEntries.length} tools.`);
